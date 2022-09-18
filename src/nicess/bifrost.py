a935103e840c989e4d6f9aad496061432aa2b82c
Gregory Tucker <gregory.tucker@gmail.com>
2022-09-18 13:46:58 +0200

    [Fix] Rowland calculations and typos

--- src/nicess/bifrost.py
+++ src/nicess/bifrost.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
-from scipp import Variable, vector, scalar, array
+from scipp import Variable, vector, scalar, array, vectors
 
 from .secondary import IndirectSecondary
 from .detectors import He3Tube
@@ -47,7 +47,7 @@ class Triplet:
         s = spacing.to(unit=position.unit)
         # tube order: (-spacing, 0, +spacing); each tube from p-l/2 to p+l/2
         tubes = [He3Tube(position - l + m * s, position + l + m * s, *pack) for m in (-1, 0, 1)]
-        return Triplet(tuple(*tubes))
+        return Triplet(tuple(tubes))
 
 
 @dataclass
@@ -89,7 +89,8 @@ class Analyzer:
         positions, taus = rowland_blades(source, position, focus, alpha, width, count)
         taus *= tau  # convert from directions to full tau vectors
 
-        shape = sum(vector(x, unit='1') * s for x, s in (([1, 0, 0], width), ([0, 1, 0], length), ([0, 0, 1], depth)))
+        edges = [vector(x, unit='1') * s for x, s in (([1., 0, 0], width), ([0., 1, 0], length), ([0., 0, 1], depth))]
+        shape = edges[0] + edges[1] + edges[2]
         blades = [Crystal(p, t, shape) for p, t in zip(positions, taus)]
         return Analyzer(tuple(blades))
 
@@ -111,14 +112,19 @@ class Channel:
             'long': array(values=[1.276, 1.388, 1.493, 1.595, 1.697], unit='m', dims=['analyzer']),
         }
         known_d_lengths = {
-            'short': array(values=[217.9, 242.0, 260.8, 279.2, 298.8], unit='mm', dims=['analyzer']),
-            'symmetric': array(values=[226.0, 249.0, 267.9, 286.3, 304.8], unit='mm', dims=['analyzer']),
-            'long': array(values=[233.9, 255.9, 274.9, 293.4, 311.9], unit='mm', dims=['analyzer']),
+            'short': vectors(values=[[0,217.9,0], [0,242.0,0], [0,260.8,0], [0,279.2,0], [0,298.8,0]], unit='mm', dims=['analyzer']),
+            'symmetric': vectors(values=[[0,226.0,0], [0,249.0,0], [0,267.9,0], [0,286.3,0], [0,304.8,0]], unit='mm', dims=['analyzer']),
+            'long': vectors(values=[[0,233.9,0], [0,255.9,0], [0,274.9,0], [0,293.4,0], [0,311.9,0]], unit='mm', dims=['analyzer']),
+        }
+        known_a_shapes = {
+            'short': vectors(values=[[0,134.0,0], [0,147.1,0], [0,156.2,0], [0,165.2,0], [0,175.6,0]], unit='mm', dims=['analyzer']),
+            'symmetric': vectors(values=[[0,142.0,0], [0,154.1,0], [0,163.2,0], [0,172.3,0], [0,181.6,0]], unit='mm', dims=['analyzer']),
+            'long': vectors(values=[[0,149.9,0], [0,161.0,0], [0,170.2,0], [0,179.3,0], [0,188.6,0]], unit='mm', dims=['analyzer']),
         }
         known_a_lengths = {
-            'short': array(values=[134, 147.1, 156.2, 165.2, 175.6], unit='mm', dims=['analyzer']),
-            'symmetric': array(values=[142, 154.1, 163.2, 172.3, 181.6], unit='mm', dims=['analyzer']),
-            'long': array(values=[149.9, 161, 170.2, 179.3, 188.6], unit='mm', dims=['analyzer']),
+            'short': array(values=[134.0, 147.1, 156.2, 165.2, 175.6], unit='mm', dims=['analyzer']),
+            'symmetric': array(values=[142.0, 154.1, 163.2, 172.3, 181.6], unit='mm', dims=['analyzer']),
+            'long': array(values=[149.9, 161.0, 170.2, 179.3, 188.6], unit='mm', dims=['analyzer']),
         }
         known_a_widths = {
             'short': array(values=[12.0, 14.0, 11.5, 12.0, 13.5], unit='mm', dims=['analyzer']),
@@ -130,14 +136,19 @@ class Channel:
             'symmetric': array(values=[2, 2, 2, 2, 2.0], unit='mm', dims=['analyzer']),
             'long': array(values=[2, 2, 2, 2, 2.0], unit='mm', dims=['analyzer']),
         }
+        known_d_spacings = {
+            'short': vectors(values=[[20.,0,0], [20.,0,0], [20.,0,0], [20.,0,0], [20.,0,0]], unit='mm', dims=['analyzer']),
+            'symmetric': vectors(values=[[20.,0,0], [20.,0,0], [20.,0,0], [20.,0,0], [20.,0,0]], unit='mm', dims=['analyzer']),
+            'long': vectors(values=[[20.,0,0], [20.,0,0], [20.,0,0], [20.,0,0], [20.,0,0]], unit='mm', dims=['analyzer']),
+        }
         counts = params.get('counts', [9, 9, 9, 7, 7])
         tau = params.get('tau', 2 * pi / params.get('dspacing', scalar(3.355, unit='angstrom')))  # PG(002)
         crystal_lengths = params.get('crystal_lengths', known_a_lengths[params.get('variant', 'symmetric')])
         crystal_widths = params.get('crystal_widths', known_a_widths[params.get('variant', 'symmetric')])
-        crystal_depths = params.get('crystal_depths', [])
+        crystal_depths = params.get('crystal_depths', known_a_depths[params.get('variant', 'symmetric')])
         detector_lengths = params.get('detector_lengths', known_d_lengths[params.get('variant', 'symmetric')])
-        detector_spacings = params.get('detector_spacings', [])
-        coverage = params.get('coverage', scalar(2, unit='degree'))
+        detector_spacings = params.get('detector_spacings', known_d_spacings[params.get('variant', 'symmetric')])
+        coverage = params.get('coverage', scalar(2., unit='degree'))
         energies = params.get('energies', array(values=[2.7, 3.2, 3.7, 4.4, 5.], unit='meV', dims=['analyzer']))
         dists_sa = params.get('sample_analyzer_distances', known_dists_sa[params.get('variant', 'symmetric')])
         dists_ad = params.get('analyzer_detector_distances', known_dists_sa['symmetric'])
@@ -145,8 +156,8 @@ class Channel:
 
         analyzer_vectors = vector([1, 0, 0], unit='1') * dists_sa
 
-        ks = sqrt(energies * 2 * neutron_mass) / hbar
-        two_thetas = 2 * asin(0.5 * tau / ks)
+        ks = (sqrt(energies * 2 * neutron_mass) / hbar).to(unit='1/angstrom')
+        two_thetas = -2 * asin(0.5 * tau / ks)
         rotations = rotations_from_rotvecs(rotation_vectors=two_thetas * vector([0, 0, 1], unit='1'))
         detector_vectors = rotations * (vector([1, 0, 0], unit='1') * dists_ad)
 
@@ -155,21 +166,34 @@ class Channel:
         analyzer_positions = sample + relative_rotation * analyzer_vectors
         detector_positions = sample + relative_rotation * (analyzer_vectors + detector_vectors)
 
-        coverages = tan(min(ks) * atan(coverage) / ks)
+        # coverages = tan(min(ks) * atan(1.0*coverage) / ks)
+        coverages = atan(min(ks) * tan(1.0*coverage) / ks)
 
         analyzers = []
         detectors = []
-        for ap, dp, ct, cl, cw, cd, cc in zip(analyzer_positions, detector_positions, counts, crystal_lengths, crystal_widths, crystal_depths, coverages):
+        for ap, dp, dl, ds, ct, cl, cw, cd, cc in zip(analyzer_positions, detector_positions, detector_lengths, detector_spacings, counts, crystal_lengths, crystal_widths, crystal_depths, coverages):
             params = dict(sample=sample, count=ct, width=cw, length=cl, depth=cd, tau=tau, coverage=cc)
             analyzers.append(Analyzer.from_calibration(ap, dp, **params))
             detectors.append(Triplet.from_calibration(dp, dl, ds))
 
-
-
-
-
+        return Channel(analyzers, detectors)
 
 
 @dataclass
 class Tank:
     channels: tuple[Channel, Channel, Channel, Channel, Channel, Channel, Channel, Channel, Channel]
+
+    @staticmethod
+    def from_calibration(**params):
+        # by default the channels are ('long', 'sym', 'short', 'long', 'sym', 'short', 'long', 'sym', 'short')
+        channel_params = [{'variant': x} for x in ('long', 'symmetric', 'short')]
+        channel_params = {i: channel_params[i%3] for i in range(9)}
+        # but this can be overridden by specifying an integer-keyed dictionary with the parameters for each channel
+        channel_params = params.get('channel_params', channel_params)
+        # The central a4 angle for each channel, relative to the reference tank angle
+        angles = params.get('angles', array(values=[-40,-30,-20,-10,  0, 10, 20, 30, 40.], unit='degree', dims=['channel']))
+
+        channels = [Channel(angles[i], **channel_parmas[i]) for i in range(9)]
+        return Tank(tuple(channels))
+
+
--- src/nicess/rowland.py
+++ src/nicess/rowland.py
@@ -1,20 +1,37 @@
 from scipp import Variable
 
 def three_point_circle(p0: Variable, p1: Variable, p2: Variable):
-    from scipp import sqrt, dot, cross, scalar
-    va = p1 - p0
-    vb = p2 - p0
-    a = sqrt(dot(va, va))
-    b = sqrt(dot(vb, vb))
+    from scipp import sqrt, dot, cross, scalar, isclose
+    # vectors along each side of the triangle
+    va, vb, vc = p1 - p0, p2 - p0, p2 - p1
+    # and their lengths
+    a, b, c = [sqrt(dot(x, x)) for x in (va, vb, vc)]
+    # the semi-perimeter, s, defines the area sqrt(s(s-a)...)
+    s = (a+b+c)/2
+    # and the circumscribing circle has radius:
+    r = a*b*c / (4*sqrt(s*(s-a)*(s-b)*(s-c)))
+    # to find the center we must define the plane normal
     n = cross(vb, va) / a / b
     if sqrt(dot(n, n)) == scalar(0., unit=n.unit):
         raise RuntimeError("Points are colinear")
-    y = cross(n, vb / b)
-    ax = dot(va, vb) / b  # |a along b|
-    ay = dot(va, y)       # |a perpendicular to b|
-    c = 0.5 * vb + ((ax / ay) * 0.5 * (ax - b) + 0.5 * ay) * y
-    r = sqrt(dot(c, c))
-    return p0 + c, r, n
+    # the bisector of any one of the sides points to the center
+    perp = [p / sqrt(dot(p,p)) for v in (-va, vb, -vc) for p in (cross(n, v),)]
+    # the length of the bisector is given by the pythagorean theorem
+    h = [sqrt(r*r - i*i/4) * p for p, i in zip(perp, (a, b, c))]
+    # but whether each h should be positive or negative is determined by the distance
+    # to the other two points
+    z = []
+    for x0, y0, z0, v, bi in zip((p0, p0, p1), (p1, p1, p2), (p2, p2, p0), (va, vb, vc), h):
+      t = x0 + 0.5*v + bi
+      if isclose(dot(t-y0, t-y0), r*r) and isclose(dot(t-z0, t-z0), r*r):
+        z.append(t)
+      else:
+        z.append(x0 + 0.5*v - bi)
+
+    # all three z points should be the same:
+    if not isclose(z[0], z[1]) or not isclose(z[1], z[2]):
+      raise RuntimeError(f"Mismatched central points,\n{p0=}\n{p1=}\n{p2=}\n{z = }")
+    return (z[0]+z[1]+z[2])/3, r, n
 
 
 def angle_between(a: Variable, b: Variable):
@@ -28,46 +45,49 @@ def transfer_angular_range(origin: Variable, target: Variable, center: Variable,
     s = origin - target
     v = s / sqrt(dot(s, s))
     c = center - target
-    n = cross(c, v) / sqrt(dot(c, c))
+    n = cross(c, v)
     if sqrt(dot(n, n)) == scalar(0., unit=n.unit):
         raise RuntimeError("vectors are colinear")
+    n = n / sqrt(dot(n, n))
     xi = center - origin
     if not isclose(dot(xi, xi), dot(c, c)):
+        print(f"{origin = }\n{target = }\n{center = }\n{alpha = }\n{xi = }\n{c = }\n{dot(xi, xi) = }\n{dot(c, c) = }")
         raise RuntimeError("s is not on the circle centered at c and passing through the origin")
     # directions perpendicular to s in the plane with c
-    hats = [sin(alpha) * x - cos(alpha) * v for x in (cross(n, v), cross(v, n))]
+    hats = [sin(alpha) * x/sqrt(dot(x, x)) - cos(alpha) * v for x in (cross(n, v), cross(v, n))]
     # points relative to target where acos(dot(p - origin, target - origin)) == alpha
-    ps = [s * h * 2 * dot(h, xi) for h in hats]
-    if not all(isclose(angle_between(p-origin, target-origin), alpha) for p in ps)
-        raise RuntimeError("Problem finding points at +- alpha")
+    ps = [origin + h * 2 * dot(h, xi) for h in hats]
+    if not all(isclose(angle_between(p-origin, target-origin), alpha) for p in ps):
+        raise RuntimeError(f"Problem finding points at +- alpha\n{ps = }\n{origin = }\n{target = }\n{center = }\n{[angle_between(p-origin, target-origin) for p in ps] = }\n{alpha = }")
     # Find the angular range relative to (target - center):
     beta = [angle_between(p - center, target-center) for p in ps]
-    return tuple(sorted(beta, key=lambda x: x.value))
+    return (beta[0] + beta[-1])/2
 
 
-def rowland_blade_angles(coverage: tuple[Variable,Variable], radius: Variable, count: int, width: Variable):
+def rowland_blade_angles(beta: Variable, radius: Variable, count: int, width: Variable):
     # the crystals cover from (min(beta), max(beta)) *around the Rowland circle center point*, rho
-    #  min(beta)                                     max(beta)
+    #    -beta                                          beta
     #  ----v------.------.------.------.------.------.---v----> rho
     #      |xxx|  |xxx|  |xxx|  |xxx|  |xxx|  |xxx|  |xxx|
     # So the angular range is broken up into N blade-width and (N-1) gap-width segments
     # The blade width is rho_blade ~= width / rowland_radius, so the gap width is given by
     #       rho_gap = (diff(beta) - N * rho_blade) / (N - 1)
     from scipp import atan2, isclose, scalar, concat
-    r_width = 2 * atan2(0.5 * width, radius.to(unit=width.unit))
-    r_gap = (coverage[1] - coverage[0] - count * r_width) / (count - 1)
-    angles = [coverage[0] + 0.5 * r_width + index * (r_width + r_gap) for index in range(count)]
+    r_width = 2 * atan2(y=0.5 * width, x=radius.to(unit=width.unit))
+    r_gap = (2*beta - count * r_width) / (count - 1)
+    angles = [-beta + 0.5 * r_width + index * (r_width + r_gap) for index in range(count)]
     if not isclose(angles[count >> 1], scalar(0., unit='radian')):
-        raise RuntimeError("Central angle should be zero but is not")
-    if not isclose(angles[-1], coverage[1] - 0.5 * r_width):
+        print(f"{beta = }\n{radius = }\n{count = }\n{width = }\n{r_width = }\n{r_gap = }\n{concat(angles,dim='blades') = }")
+        raise RuntimeError(f"Central angle should be zero but is {angles[count>>1]}.")
+    if not isclose(angles[-1], beta - 0.5 * r_width):
         raise RuntimeError("Last angle should be half a radial-width from the maximum coverage angle!")
     return concat(angles, 'blade')
 
 
 def rowland_blades(source: Variable, position: Variable, focus: Variable, alpha: Variable, width: Variable, count: int):
     center, radius, normal = three_point_circle(source, position, focus)
-    betas = transfer_angular_range(source, position, center, alpha)
-    angles = rowland_blade_angles(betas, radius, count, width)
+    beta = transfer_angular_range(source, position, center, alpha)
+    angles = rowland_blade_angles(beta, radius, count, width)
 
     # for each angle, create the rotation matrix needed to rotate (position - center)
     from scipp.spatial import rotations_from_rotvecs
